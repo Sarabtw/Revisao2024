8a03736d77f46559db5bca30b3c6281915c4b1e2
Sara Monteiro <saramonteiro2008@gmail.com>
2025-01-20 14:38:04 -0300

    Fases iniciais gerador de senha

---
+++ geradorSenha/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Criador de senha ");
+
+Console.WriteLine("Insira a quantidade de caracteres que deseja em sua senha: (Ps: Máximo 10 e minimo 4)");
+if (int.TryParse(Console.ReadLine(), out int caracteres)) ;
+{
+    Console.WriteLine("Você deseja adicionar números na sua senha?");
+    string numeros = Console.ReadLine();
+
+    Console.WriteLine("Você deseja inserir letras na senha?");
+    string letras = Console.ReadLine();
+
+    Console.WriteLine("Você deseja adicionar caracteres especiais?(Ex: @, !, # ou -)");
+    string especiais = Console.ReadLine();
+
+    Random random = new Random();
+
+    if (caracteres <= 4)
+    {
+        Console.WriteLine("Não é possivel criar a sua senha (ERRO).");
+    }
+    if (caracteres >= 10)
+    {
+        Console.WriteLine("Não é possivel criar a sua senha (ERRO).");
+    }
+}
