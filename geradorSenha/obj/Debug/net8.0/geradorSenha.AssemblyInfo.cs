34ee649d3e0ed4785d33ac6afd2b6ee1baf8e0a4
Sara Monteiro <saramonteiro2008@gmail.com>
2025-01-21 14:51:57 -0300

    Finalizando código

--- geradorSenha/obj/Debug/net8.0/geradorSenha.AssemblyInfo.cs
+++ geradorSenha/obj/Debug/net8.0/geradorSenha.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("geradorSenha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bbf11353423a13426102b30b5804f8809277107")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5efd6e620cc23a5295976f11bf7071ccda942f38")]
 [assembly: System.Reflection.AssemblyProductAttribute("geradorSenha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("geradorSenha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
